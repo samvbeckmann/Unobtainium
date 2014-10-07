fed3a5cea0ddc925e22ab14f6baff9c634792056
Sam Beckmann <samvbeckmann@gmail.com>
2014-10-07 15:54:09 -0500

    Set version of FML to current recommended, added mod class.

---
+++ src/main/java/com/qkninja/unobtainium/Unobtainium.java
@@ -0,0 +1,37 @@
+package com.qkninja.unobtainium;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * @author QKninja
+ */
+
+@Mod(modid="Unobtainium", name="Unobtainium", version="1.7.10-1.0")
+public class Unobtainium
+{
+
+    @Mod.Instance("Unobtainium")
+    public static Unobtainium instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
