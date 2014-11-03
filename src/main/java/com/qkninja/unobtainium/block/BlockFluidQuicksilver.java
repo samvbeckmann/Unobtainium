95be4bde629d1a37ec5bcf8508be2dc3aee941d4
Sam Beckmann <svb761@utulsa.edu>
2014-11-03 00:44:35 -0600

    Added poison effect to Quicksilver

--- src/main/java/com/qkninja/unobtainium/block/BlockFluidQuicksilver.java
+++ src/main/java/com/qkninja/unobtainium/block/BlockFluidQuicksilver.java
@@ -3,6 +3,10 @@ package com.qkninja.unobtainium.block;
 import com.qkninja.unobtainium.init.ModFluidBlocks;
 import com.qkninja.unobtainium.reference.Names;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
 
@@ -27,6 +31,14 @@ public class BlockFluidQuicksilver extends BlockFluidUnobtainiumMod
         {
             world.setBlock(x, y+ densityDir, z, ModFluidBlocks.blockQuicksilver);
             world.setBlockToAir(x, y, z);
+        }
+    }
+
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity player)
+    {
+        if (player instanceof EntityLivingBase)
+        {
+            ((EntityLivingBase)player).addPotionEffect(new PotionEffect(Potion.poison.getId(), 400, 2));
         }
     }
 }
