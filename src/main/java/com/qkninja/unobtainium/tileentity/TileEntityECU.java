f877e5417d026aaabeef5bd22bee46a7eeaf3be1
samvbeckmann <samvbeckmann@gmail.com>
2015-03-20 23:14:12 -0500

    ECU working as intended.

--- src/main/java/com/qkninja/unobtainium/tileentity/TileEntityECU.java
+++ src/main/java/com/qkninja/unobtainium/tileentity/TileEntityECU.java
@@ -1,18 +1,12 @@
 package com.qkninja.unobtainium.tileentity;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
-
-import java.util.Random;
 
 /**
  * ECU speeds up various processes and can rapidly cool down materials.
  */
 public class TileEntityECU extends TileEntityUnobtainium
 {
-    private Random rnd = new Random();
-
     public TileEntityECU()
     {
         super();
@@ -21,34 +15,49 @@ public class TileEntityECU extends TileEntityUnobtainium
     @Override
     public void updateEntity()
     {
-        int yOffset = rnd.nextInt(10) - 5;
-        int xOffset = rnd.nextInt(10) - 5;
-        int zOffset = rnd.nextInt(10) - 5;
-
-        if ((xOffset != 0 || yOffset != 0 || zOffset != 0) &&
-                (Math.abs(xOffset) + Math.abs(yOffset) + Math.abs(zOffset) <= 5))
+        if (!worldObj.isRemote)
         {
-            checkAndReplaceLava(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset);
+            int yOffset = worldObj.rand.nextInt(10) - 5;
+            int xOffset = worldObj.rand.nextInt(10) - 5;
+            int zOffset = worldObj.rand.nextInt(10) - 5;
+
+            if ((xOffset != 0 || yOffset != 0 || zOffset != 0) &&
+                    (Math.abs(xOffset) + Math.abs(yOffset) + Math.abs(zOffset) <= 5))
+            {
+                checkAndReplaceLava(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset);
+                checkAndReplaceWater(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset);
+            }
         }
     }
 
     /**
      * Checks given block to see if it's lava. If so, replaces that block with the appropriate cooled material.
+     *
      * @param x x coord
      * @param y y coord
      * @param z z coord
      */
-    private void checkAndReplaceLava(int x, int y, int z) // TODO: a bit buggy
+    private void checkAndReplaceLava(int x, int y, int z)
     {
-        Block block = worldObj.getBlock(x, y, z);
-
-        if (block.getMaterial() == Material.lava)
+        if (worldObj.getBlock(x,y,z).equals(Blocks.lava))
         {
-            if (block.equals(Blocks.flowing_lava))
-            {
-                worldObj.setBlock(x, y, z, Blocks.stone);
-            } else if (block.equals(Blocks.lava))
+            if (worldObj.getBlockMetadata(x, y, z) == 0)
                 worldObj.setBlock(x, y, z, Blocks.obsidian);
+            else
+                worldObj.setBlock(x,y,z, Blocks.stone);
         }
+    }
+
+    /**
+     * Checks a given block to see if it's a water source block. If so, replaces that block with ice.
+     *
+     * @param x x coord
+     * @param y y coord
+     * @param z z coord
+     */
+    private void checkAndReplaceWater(int x, int y, int z)
+    {
+        if (worldObj.getBlock(x,y,z).equals(Blocks.water) && worldObj.getBlockMetadata(x,y,z) == 0)
+            worldObj.setBlock(x,y,z, Blocks.ice);
     }
 }
